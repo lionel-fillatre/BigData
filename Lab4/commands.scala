7f8fe8b67882e06334978c2875a814c1ee524b5d
lionel-fillatre <161297545+lionel-fillatre@users.noreply.github.com>
2024-10-03 17:13:09 +0200

    commands add comments

--- Lab4/commands.scala
+++ Lab4/commands.scala
@@ -1,21 +1,21 @@
 // import the library to run linux shell commands inside the spark shell
 import scala.sys.process._
+//
 // for example, you can run the following linux shell commands: 
 "ls -l /opt/spark/work-dir/lab4".!
 "cat /opt/spark/work-dir/lab4/color.txt".!
-
+//
 // first command
 val files = sc.wholeTextFiles("/opt/spark/work-dir/lab4/*.txt").collect.toList 
-
+//
 // second command
 var ch = "hello a b  c   bye"
 ch.split("\\s+")
 ch.split(" ")
-
 // third command
 var fruit = Array(("apple","2"), ("orange","3"), ("pear","1"))
 fruit(2)._2
-
+//
 // fourth command
 val a=Array((1,2),(3,4),(3,6))
 val b=Array((3,9))
